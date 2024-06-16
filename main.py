b577da6658dac5286f0c6b87a6f82ca5a6276918
逆风少年 <202632144@qq.com>
2024-06-17 05:19:49 +0800

    delete silicon.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from api.hello import router as hello_router
 from api.random import router as random_router
 from api.v1.groq import router as groq_router
 from api.sd3 import router as sd3_router
-from api.silicon import router as silicon_router
+
 from fastapi import FastAPI
 from fastapi.responses import Response
 app = FastAPI()
@@ -13,7 +13,7 @@ app.include_router(hello_router, prefix="/hello")
 app.include_router(random_router, prefix="/random")
 app.include_router(groq_router, prefix="/v1/groq")
 app.include_router(sd3_router, prefix="/sd3")
-app.include_router(silicon_router, prefix="/silicon")
+
 
 
 @app.get("/")
